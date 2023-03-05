983bbeece6ad4e24900a39a85a4cdbfe32220c01
alex <lantlot@gmail.com>
2023-03-06 01:03:25 +0800

    修改请求方式

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -12,5 +12,4 @@ def index():
     :return: 返回index页面
     """
     params = request.get_json()
-    print(params)
-    return requests.post("https://www.0x3f.top/chat",params)
+    return requests.post(url="https://www.0x3f.top/chat", json=params)
