e27e596db9b6b78e52af108c2b09754bcbaac575
alex <lantlot@gmail.com>
2023-03-14 00:04:46 +0800

    增加异步创建/拉取消息接口

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,3 +1,5 @@
+import threading
+import uuid
 from datetime import datetime
 
 import requests
@@ -6,16 +8,69 @@ from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+class MessageDict(dict):
+    def __missing__(self, key):
+        return ""
+
+    def __getitem__(self, item):
+        return super(MessageDict, self).__getitem__(item)
+
+    def get(self, k, d=None):
+        return super(MessageDict, self).get(k, d)
+
+message_dict=MessageDict()
+
 @app.route('/chat',methods=["POST"])
 def index():
     """
     :return: 返回index页面
     """
     params = request.get_json()
-    res=requests.post(url="https://www.0x3f.top/chat", json=params).text
-    return  res
+    res = requests.post(url="https://www.0x3f.top/chat", json=params).text
+    return res
+@app.route('/createMessage',methods=["POST"])
+def create_message():
+    params = request.get_json()
+    message_id = uuid.uuid4().hex
+    thread = threading.Thread(target=get_chat_message, args=(params,message_id))
+    thread.start()
+    return  make_succ_response(uuid)
+
+@app.route('/pullMessage',methods=["GET"])
+def pull_message():
+    """
+    :return: 返回index页面
+    """
+    params = request.get_json()
+    message_id = uuid.uuid4().hex
+    thread = threading.Thread(target=get_chat_message, args=(params,message_id))
+    thread.start()
+    return  make_succ_response(uuid)
+
+
+def get_chat_message(params, message_id):
+    res = requests.post(url="https://www.0x3f.top/chat", json=params).text
+    message_dict[message_id]=res
+
+
 @app.route('/adv',methods=["POST"])
 def adv():
     params = request.get_json()
     res=requests.post(url="https://www.0x3f.top/adv", json=params).text
     return  res
+
+
+@app.route('/createAdv',methods=["POST"])
+def create_adv_message():
+    params = request.get_json()
+    message_id = uuid.uuid4().hex
+    thread = threading.Thread(target=get_adv_message, args=(params, message_id))
+    thread.start()
+    return make_succ_response(uuid)
+
+
+def get_adv_message(params):
+    res = requests.post(url="https://www.0x3f.top/adv", json=params).text
+    return res
+
+
